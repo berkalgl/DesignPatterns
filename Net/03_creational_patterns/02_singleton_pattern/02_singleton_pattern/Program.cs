c05eca7a39174c92085d693d9c36d55907637d7c
berkalgl <berkalgull@gmail.com>
2022-09-20 22:24:17 +0300

    Singleton for net

---
+++ Net/03_creational_patterns/02_singleton_pattern/02_singleton_pattern/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02_singleton_pattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread process1 = new Thread(() =>
+            {
+                TestSingleton();
+            });
+            Thread process2 = new Thread(() =>
+            {
+                TestSingleton();
+            });
+
+            process1.Start();
+            process2.Start();
+
+            process1.Join();
+            process2.Join();
+
+        }
+        public static void TestSingleton()
+        {
+            Singleton singleton = Singleton.GetInstance();
+        }
+    }
+}
---
+++ Net/03_creational_patterns/02_singleton_pattern/02_singleton_pattern/Singleton.cs
@@ -0,0 +1,42 @@
+﻿namespace _02_singleton_pattern
+{
+    // This Singleton implementation is called "double check lock". It is safe
+    // in multithreaded environment and provides lazy initialization for the
+    // Singleton object.
+    public class Singleton
+    {
+        private static Singleton? _instance;
+
+        // We now have a lock object that will be used to synchronize threads
+        // during first access to the Singleton.
+        private static readonly Object _lock = new object();
+
+        public Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            // This conditional is needed to prevent threads stumbling over the
+            // lock once the instance is ready.
+            if (_instance == null)
+            {
+                // Now, imagine that the program has just been launched. Since
+                // there's no Singleton instance yet, multiple threads can
+                // simultaneously pass the previous conditional and reach this
+                // point almost at the same time. The first of them will acquire
+                // lock and will proceed further, while the rest will wait here.
+                lock (_lock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
+
